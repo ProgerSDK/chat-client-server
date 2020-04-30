29d2d458c23f1cdcc71cd0fb2ab8907847a877bf
Volodya Taras <proger.sdk@gmail.com>
2020-04-30 14:14:04 +0300

    Server: Add check_command

--- client/main.py
+++ client/main.py
@@ -1,8 +1,9 @@
 import sys
 import socket
-
-HOST = '127.0.0.1'  # Standard server's IP address
-PORT = 9090        # Standard port used by the server
+import constants
+import struct
+import time
+import config
 
 
 # Read args from user
@@ -11,11 +12,24 @@ data = " ".join(sys.argv[1:])
 # Create a socket (SOCK_STREAM means a TCP socket)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     # Connect to server and send data
-    sock.connect((HOST, PORT))
-    sock.sendall(bytes(data + "\n", "utf-8"))
+    sock.connect((config.HOST, config.PORT))
+    ping = constants.CMD_RECEIVE_FILE
 
+    ping_message = struct.pack('b', ping)
+    message = bytearray('Hello!!!', config.ENCODING)
+    size = struct.pack('i', len(message)+1)
+
+    sock.send(size)
+    time.sleep(1)
+
+    sock.send(ping_message + message)
+    time.sleep(1)
+    
     # Receive data from the server and shut down
-    received = str(sock.recv(1024), "utf-8")
+    received = str(sock.recv(1024), config.ENCODING)
+
+    if (input()):
+        pass
 
 print("Sent:     {}".format(data))
 print("Received: {}".format(received)) 
--- server/main.py
+++ server/main.py
@@ -2,27 +2,49 @@ import socket
 import threading
 import socketserver
 import time
+import config
+import struct
 
-HOST = '127.0.0.1'  # standard ip
-PORT = 9090         # standard port
+
+def check_command(message):
+    # get user command from message
+    print(f'Command: {message[0]}')
+
+    # if there is content other than the command
+    if (len(message) > 1):
+        # print(len(message))
+        content = message[1:]
+        print(f'Content: {str(content, config.ENCODING)}')
 
 
 class RequestHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
-        data = self.request.recv(1024)
+        # get size of user message
+        size = self.request.recv(4)
+        size_of_message = struct.unpack('i', size)
+        
+        print(f'\n\nSize of message: {size_of_message[0]}')
+        
+        # get user message
+        message = self.request.recv(size_of_message[0])
+        # and check command in message
+        check_command(message)
+
+        # send response
         cur_thread = threading.current_thread()
-        response = bytes("{}: {}".format(cur_thread.name, data), 'utf-8')
+        response = bytes("{}: {}".format(cur_thread.name, message), config.ENCODING)
         self.request.sendall(response)
 
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    allow_reuse_address = True
     pass
 
 
 if __name__ == '__main__':
-    host = HOST
-    port = PORT
+    host = config.HOST
+    port = config.PORT
 
     server = ThreadedTCPServer((host, port), RequestHandler)
     with server:    
@@ -37,7 +59,7 @@ if __name__ == '__main__':
         
         # Exit when user enters something
         while True:
-            if (input('Enter something to exit:\n-> ')):
+            if (input('Enter something to exit: -> ')):
                 break
 
         server.shutdown()
